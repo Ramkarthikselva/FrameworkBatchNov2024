b9187619fd5c9b9546cf67864ed7aa0b2c6a299a
Ramkarthikselva <ramkarthikselva@gmail.com>
2024-11-23 15:09:59 +0530

    Test

---
+++ src/test/java/com/gitTest/GitTool.java
@@ -0,0 +1,25 @@
+package com.gitTest;
+
+import java.security.PublicKey;
+
+public class GitTool {
+	
+	public void name() {
+		System.out.println("Karthik");
+	}
+	public void id() {
+		System.out.println("1234");
+	}
+	public void gender() {
+		System.out.println('M');
+		
+	}
+	public static void main(String[] args) {
+		GitTool g= new GitTool();
+		g.name();
+		g.id();
+		g.gender();
+		
+	}
+
+}
